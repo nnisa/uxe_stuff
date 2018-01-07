6b747e9d9eabd85f62e89361b74031292e02ad35
Noshin Anjum <noshin@Noshins-MacBook-Pro.local>
2018-01-07 11:50:02 -0500

    the time difference function is not working

--- scroll+swipe/script.js
+++ scroll+swipe/script.js
@@ -2,6 +2,12 @@ const api = "http://message-list.appspot.com";
 var page_token;
 var first_load = "/messages";
 
+
+
+// window.onload = function() {
+//     document.getElementById('messages').className += " loaded";
+// }
+
 //loading JSON object based on the url, it loads the first object and changes the URL moving forward to new tokens
 function table(link){
     $.getJSON(api + link, function(message_object){
@@ -22,6 +28,7 @@ function create_new_messages(message_object){
     var date_time = message_object.messages[i].updated;
     var date = date_time.slice(0,10);
     var time = date_time.slice(11,19);
+    // console.log(date_time);
     var id = message_object.messages[i].id;
     var messages_section = document.getElementById('messages')
 
@@ -59,75 +66,67 @@ if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
     }
 };
 
+function timeDifference(current_date, input_date){
+  var current_date = new Date(current_date);
+  var input_date = new Date(input_date);
+  
+  var date_difference = input_date.getTime() - current_date.getTime();
+  var milli_sec = date_difference;
+  // getting hour
+  var hour = Math.floor(milli_sec / 1000 / 60 / 60);
+  milli_sec -=  hour * 1000 * 60 * 60;
+  //getting minutes
+  var minute = Math.floor(milli_sec / 1000 / 60);
+  milli_sec -= minute * 1000 * 60;
+  //getting second
+  var second = Math.floor(milli_sec / 1000);
+  milli_sec -= second * 1000;
 
-
-// is the date today?
-//   yes 
-//     check time 
-//       minutes back or more than equal to 60 min 
-//         hours back  
-//   No 
-//     check days difference 
-//       equal to 1? 
-//         yes : yesterday 
-//       no : get which day 
-//       more than equal to 
-
-
-
-
+  return (hour, minute, second);
+}
 
 
 function checkDate(){
   var month = new Array();
   month = ["January","February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
-  var inputDate = "2015-02-01";
   var todaysDate = new Date();
-  var current_day = todaysDate.getDay();
+  var current_day = todaysDate.getDate();
   var current_month = todaysDate.getMonth();
   var current_year = todaysDate.getFullYear();
   
-  var milliseconds = date.getTime();
-  var seconds = milliseconds / 1000;
-  
+  var inputDate = "2018-01-08T11:36:23Z";
   var input_day = parseInt(inputDate.slice(8,10));
   var input_month = parseInt(inputDate.slice(5,7));
   var input_year = parseInt(inputDate.slice(0,4));
 
+  var current_date = current_year + "-" +(current_month+1)+ "-" +current_day + " " + todaysDate.getHours() + ":" +todaysDate.getMinutes()+ ":" +todaysDate.getSeconds(); 
+  var message_input_date = inputDate.slice(0,10)+ " " +inputDate.slice(12,19);
+  var hour, minute, second;
 
 
-  if (input_year == current_year) { // this year
-    if(input_month == current_month){ // this month
-      if (input_day == current_day){ // today
+  // if (input_year == current_year) { // this year
+  //   if(input_month == current_month){ // this month
+  //     if (input_day == current_day){ // today
+  //       var hour, minute, second = timeDifference(current_date, message_input_date);
 
 
-
-
-
-
-
-      } else {
-        if ((todaysDate - input_day)=1){// not today but yesterday 
-          return ("Yesterday")
-        } else { // not yesterday 
-          return (month[input_month-1]input_month + " " + input_day)
-        } 
-      }
-    } else { // not this month
-      return (month[input_month-1]input_month + " " + input_day)
-    }
-  } else { // nor this year
-      return (input_month + "/" + input_day + "/" + input_year) 
-  }
-
-  console.log()
-
+  //     } else {
+  //       return (month[input_month-1]input_month + " " + input_day)
+  //     }
+  //   } else { // not this month
+  //     return (month[input_month-1]input_month + " " + input_day)
+  //   }
+  // } else { // nor this year
+  //     return (input_month + "/" + input_day + "/" + input_year) 
+  // }
+  hour, minute, second = timeDifference(current_date, message_input_date);
+  console.log(hour)
+  
 }
 
 
 checkDate();
-
 
 
 
