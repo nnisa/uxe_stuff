b427a60aa75c1a07742085e1d187f8769f382de2
Noshin Anjum <noshin@Noshins-MacBook-Pro.local>
2018-01-06 23:29:03 -0500

    added bit of the time related code

--- scroll+swipe/script.js
+++ scroll+swipe/script.js
@@ -25,26 +25,24 @@ function create_new_messages(message_object){
     var id = message_object.messages[i].id;
     var messages_section = document.getElementById('messages')
 
-    // $(`<div id = ${id} class = "per_message_div">`+
-    //   `<table>`+
-    //     `<tr>`+
-    //       `<td>`+
-    //         `<div class = "image_border">`+
-    //           `<img class = "profile_image" src= ${profile_image_url}>`+
-    //         `</div>`+
-    //       `</td>`+
-    //       `<td>`+
-    //         `<div class = "content_section">`+
-    //           `<p class = "author_name"> ${author_name}</p>`+
-    //           `<div class = "date_section">`+
-    //             `<p class = "date">${time}</p>`+
-    //           `</div>`+
-    //         `</div>`+
-    //       `</td>`+
-    //     `</tr>`+
-    //     `<p class = "author_content">${author_content}</p>`+
-    //   `</table>`+
-    // `</div>`).appendTo($(messages_section));
+    $(`<div id = ${id} class = "notification_card">`+
+      `<table>`+
+        `<tr>`+
+          `<td>`+
+            `<div class = "image_border">`+
+              `<img class = "profile_image" src= ${profile_image_url}>`+
+            `</div>`+
+          `</td>`+
+          `<td>`+
+            `<div class = "content_section">`+
+              `<p class = "author_name">${author_name}</p>`+
+              `<p class = "date">${date}</p>`+
+            `</div>`+
+          `</td>`+
+        `</tr>`+
+      `</table>`+
+      `<p class = "author_content">${author_content}</p>`+
+    `</div>`).appendTo($(messages_section));
   }
 }
 
@@ -60,3 +58,82 @@ if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
     table(page_token);
     }
 };
+
+
+
+// is the date today?
+//   yes 
+//     check time 
+//       minutes back or more than equal to 60 min 
+//         hours back  
+//   No 
+//     check days difference 
+//       equal to 1? 
+//         yes : yesterday 
+//       no : get which day 
+//       more than equal to 
+
+
+
+
+
+
+function checkDate(){
+  var month = new Array();
+  month = ["January","February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+  var inputDate = "2015-02-01";
+  var todaysDate = new Date();
+  var current_day = todaysDate.getDay();
+  var current_month = todaysDate.getMonth();
+  var current_year = todaysDate.getFullYear();
+  
+  var milliseconds = date.getTime();
+  var seconds = milliseconds / 1000;
+  
+  var input_day = parseInt(inputDate.slice(8,10));
+  var input_month = parseInt(inputDate.slice(5,7));
+  var input_year = parseInt(inputDate.slice(0,4));
+
+
+
+  if (input_year == current_year) { // this year
+    if(input_month == current_month){ // this month
+      if (input_day == current_day){ // today
+
+
+
+
+
+
+
+      } else {
+        if ((todaysDate - input_day)=1){// not today but yesterday 
+          return ("Yesterday")
+        } else { // not yesterday 
+          return (month[input_month-1]input_month + " " + input_day)
+        } 
+      }
+    } else { // not this month
+      return (month[input_month-1]input_month + " " + input_day)
+    }
+  } else { // nor this year
+      return (input_month + "/" + input_day + "/" + input_year) 
+  }
+
+  console.log()
+
+}
+
+
+checkDate();
+
+
+
+
+
+
+
+
+
+
